1ed6d1132f813a94ccef3bd82777b317e1490022
L0crian <mailjustmy@gmail.com>
2017-04-29 05:46:19 +0300

    First commit Social-Network

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Session;
+use App\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    public function index($slug)
+    {
+        $user = User::where('slug', $slug)->first();
+
+        return view('profiles/profile')->with('user', $user);
+    }
+
+    public function edit()
+    {
+        return view('profiles/edit')->with('info', Auth::user()->profile);
+    }
+
+
+
+    public function update(Request $request)
+    {
+
+        $this->validate($request, [
+            'location' => 'required',
+            'about' => 'required|max:255'
+        ]);
+
+        if($request->hasFile('avatar')){
+            Auth::user()->update([
+               'avatar' => $request->avatar->store('public/avatars')
+            ]);
+        }
+
+        Auth::user()->profile->update([
+           'location' => $request->input('location'),
+            'about' => $request->input('about')
+        ]);
+
+        Session::flash('succes', 'Profile updated');
+
+        return redirect()->back();
+    }
+}
